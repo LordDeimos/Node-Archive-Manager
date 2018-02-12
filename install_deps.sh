38ef23bd6d0fc563541340e64ef8dab3b7bb9dfb
Alex Barnier <alexjbarnier@gmail.com>
2018-02-13 07:49:24 +1000

    Update travis' deps

--- install_deps.sh
+++ install_deps.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-wget http://launchpadlibrarian.net/314907453/gcc-6-base_6.3.0-12ubuntu2_amd64.deb
-sudo dpkg -i gcc-6-base_6.3.0-12ubuntu2_amd64.deb
+wget http://launchpadlibrarian.net/333811953/gcc-7-base_7.2.0-1ubuntu1_amd64.deb
+sudo dpkg -i gcc-7-base_7.2.0-1ubuntu1_amd64.deb
 
-wget http://launchpadlibrarian.net/314907683/libgcc1_6.3.0-12ubuntu2_amd64.deb
-sudo dpkg -i libgcc1_6.3.0-12ubuntu2_amd64.deb
+wget http://launchpadlibrarian.net/340938701/libgcc1_7.2.0-8ubuntu3_arm64.deb
+sudo dpkg -i libgcc1_7.2.0-8ubuntu3_arm64.deb
 
-wget http://launchpadlibrarian.net/153554145/libc6_2.17-93ubuntu4_amd64.deb
-sudo dpkg -i libc6_2.17-93ubuntu4_amd64.deb
+wget http://launchpadlibrarian.net/340569656/libc6_2.26-0ubuntu2_amd64.deb
+sudo dpkg -i libc6_2.26-0ubuntu2_amd64.deb
 
-wget http://launchpadlibrarian.net/314907710/libstdc++6_6.3.0-12ubuntu2_amd64.deb
-sudo dpkg -i libstdc++6_6.3.0-12ubuntu2_amd64.deb
+wget http://launchpadlibrarian.net/340928702/libstdc++6_7.2.0-8ubuntu3_amd64.deb
+sudo dpkg -i libstdc++6_7.2.0-8ubuntu3_amd64.deb
 
-wget http://launchpadlibrarian.net/184377490/libattr1_2.4.47-2_amd64.deb
-sudo dpkg -i libattr1_2.4.47-2_amd64.deb
+wget http://launchpadlibrarian.net/316560019/libattr1_2.4.47-2build1_amd64.deb
+sudo dpkg -i libattr1_2.4.47-2build1_amd64.deb
 
 wget http://launchpadlibrarian.net/236856983/libacl1_2.2.52-3_amd64.deb
 sudo dpkg -i libacl1_2.2.52-3_amd64.deb
@@ -30,21 +30,22 @@ sudo dpkg -i liblzma5_5.2.2-1.2_amd64.deb
 wget http://launchpadlibrarian.net/193137239/liblzo2-2_2.08-1.2_amd64.deb
 sudo dpkg -i liblzo2-2_2.08-1.2_amd64.deb
 
-wget http://launchpadlibrarian.net/291731898/libnettle6_3.3-1_amd64.deb
-sudo dpkg -i libnettle6_3.3-1_amd64.deb
+wget http://launchpadlibrarian.net/340328869/libnettle6_3.3-2_amd64.deb
+sudo dpkg -i libnettle6_3.3-2_amd64.deb
 
-wget http://launchpadlibrarian.net/341173976/libicu57_57.1-5ubuntu0.2_amd64.deb
-sudo dpkg -i libicu57_57.1-5ubuntu0.2_amd64.deb
+wget http://launchpadlibrarian.net/317614660/libicu57_57.1-6_amd64.deb
+sudo dpkg -i libicu57_57.1-6_amd64.deb
 
 wget http://launchpadlibrarian.net/306702009/zlib1g_1.2.11.dfsg-0ubuntu1_amd64.deb
 sudo dpkg -i zlib1g_1.2.11.dfsg-0ubuntu1_amd64.deb
 
 wget http://launchpadlibrarian.net/348948625/libxml2_2.9.4+dfsg1-2.2ubuntu0.3_amd64.deb
+sudo dpkg -i libxml2_2.9.4+dfsg1-2.2ubuntu0.3_amd64.deb
 
-wget http://launchpadlibrarian.net/314712550/libarchive13_3.2.2-2_amd64.deb
-sudo dpkg -i libarchive13_3.2.2-2_amd64.deb
+wget http://launchpadlibrarian.net/337038566/libarchive13_3.2.2-3.1_amd64.deb
+sudo dpkg -i libarchive13_3.2.2-3.1_amd64.deb
 
-wget http://launchpadlibrarian.net/314712540/libarchive-dev_3.2.2-2_amd64.deb
-sudo dpkg -i libarchive-dev_3.2.2-2_amd64.deb
+wget http://launchpadlibrarian.net/337038560/libarchive-dev_3.2.2-3.1_amd64.deb
+sudo dpkg -i libarchive-dev_3.2.2-3.1_amd64.deb
 
 rm -f *.deb
